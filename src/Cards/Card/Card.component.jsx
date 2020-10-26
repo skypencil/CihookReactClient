e68acc5557acf984168f8f08e94e225423b44dfd
skypencil <yaquby.samim@gmail.com>
2020-10-26 13:44:26 -0400

    Carousel Card Added

--- src/Cards/Card/Card.component.jsx
+++ src/Cards/Card/Card.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Carousel from './Carousel/Carousel.component.jsx'
 import './Card.style.css';
 
 const Card = (props) => { 
     return(
         <div className="Card">
+            <div className="CarouselContainer">
+                <Carousel/>
+            </div>
             some text
         </div>
     )
---
+++ src/Cards/Card/Carousel/Carousel.component.jsx
@@ -0,0 +1,16 @@
+import React, { userState } from 'react';
+import './Carousel.style.css'
+
+/**
+ * @function Carousel
+ */
+
+const Carousel = () => { 
+    return (
+        <div className="Carousel">
+
+        </div>
+    )
+}
+
+export default Carousel;
